6f2cbf061a556aac4ccb51b74958a68eb83d293d
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-07-16 20:36:29 -0500

    initial commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import RecipeCollection from './RecipeCollection';
+
+function App() {
+  return (
+    <div className="App">
+      <RecipeCollection />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/containers/RecipeCollection.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class RecipeCollection extends Component {
+    render() {
+        return (
+            <div>
+                <header>EPICER</header>
+                <body>
+                    wait for me, my sweet
+                </body>
+            </div>
+        );
+    }
+}
+
+export default RecipeCollection;
